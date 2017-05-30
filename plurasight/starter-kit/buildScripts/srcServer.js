bdef393aea8752af17b36ce866ea9d6a5b7e302b
technancy <nancytechy@gmail.com>
2017-05-30 11:41:42 +0100

    starter-kit: demo basic express api

--- plurasight/starter-kit/buildScripts/srcServer.js
+++ plurasight/starter-kit/buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1, "firstName":"Bob"},
+    {"id": 2, "firstName":"Bobby"},
+    {"id": 3, "firstName":"Bobbins"}
+  ]);
+});
+
 app.listen(port, function(err){
   if (err) {
     console.log('Site port error');
--- plurasight/starter-kit/src/index.js
+++ plurasight/starter-kit/src/index.js
@@ -1,5 +1,17 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('0,0.00');
-console.log(`I would pay £${courseValue} for this course.`); // eslint-disable-line no-console
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+});
